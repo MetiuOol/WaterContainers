84202399c3444a466c71832eb94d7264d73002eb
Mateusz Wąsiński <wasinski.mateusz@gmail.com>
2024-04-04 18:40:00 +0200

    Added WaterContainer model with basic methods and constructors

---
+++ src/main/java/pl/kurs/watercontainers/models/WaterContainer.java
@@ -0,0 +1,57 @@
+package pl.kurs.watercontainers.models;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class WaterContainer implements Serializable {
+    static final long serialVersionUID = 1L;
+
+    private final String name;
+    private final double maxCapacity;
+    private double waterLevel;
+
+    public WaterContainer(String name, double maxCapacity, double waterLevel) {
+        this.name = name;
+        this.maxCapacity = maxCapacity;
+        this.waterLevel = waterLevel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public double getWaterLevel() {
+        return waterLevel;
+    }
+
+    public void setWaterLevel(double waterLevel) {
+        this.waterLevel = waterLevel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WaterContainer that = (WaterContainer) o;
+        return Double.compare(that.maxCapacity, maxCapacity) == 0 && Double.compare(that.waterLevel, waterLevel) == 0 && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, maxCapacity, waterLevel);
+    }
+
+    @Override
+    public String toString() {
+        return "WaterContainer{" +
+                "name='" + name + '\'' +
+                ", maxCapacity=" + maxCapacity +
+                ", waterLevel=" + waterLevel +
+                '}';
+    }
+}
