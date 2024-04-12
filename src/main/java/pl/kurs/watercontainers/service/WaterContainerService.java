8fc2bd6ad1658a7dc19fc59d92fef039dad038c6
Mateusz Wąsiński <wasinski.mateusz@gmail.com>
2024-04-12 20:49:55 +0200

    Added new method to WaterContainerService.

--- src/main/java/pl/kurs/watercontainers/service/WaterContainerService.java
+++ src/main/java/pl/kurs/watercontainers/service/WaterContainerService.java
@@ -15,7 +15,23 @@ public class WaterContainerService {
         } catch (RuntimeException e) {
             e.printStackTrace();
         }
-            return highestWaterLevelContainer;
+        return highestWaterLevelContainer;
 
     }
+
+    public static WaterContainer getTheMostFullContainer(WaterContainer... containers) {
+        WaterContainer theMostFullContainer = null;
+        try {
+            theMostFullContainer = containers[0];
+            for (WaterContainer container : containers) {
+                if ((theMostFullContainer.getWaterLevel() / theMostFullContainer.getMaxCapacity()) < (container.getWaterLevel() / container.getMaxCapacity()))
+                    theMostFullContainer = container;
+            }
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+        return theMostFullContainer;
+
+    }
+
 }
